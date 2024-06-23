a3f1e0b383ebf8e942af04aa46c085b8194fbfbb
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2024-06-23 10:59:40 +0530

    name fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer= () => {
     return(
         <div className="mt-10">
             <hr className="border-2 border-gray-700 w-11/12 m-auto"/>
-            <div className="text-center text-xl text-gray-700 font-semibold mt-5 mb-4">FlavourFusion Copyright © 2023 FlavourFusion - All rights reserved || Designed By: Naman & Bhavesh</div>
+            <div className="text-center text-xl text-gray-700 font-semibold mt-5 mb-4">Food Network Copyright © 2023 Food Network - All rights reserved || Developed By: Pratyaksh</div>
         </div>
     )
 }
